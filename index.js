3d7e3b8a29ce041e2954756b80a81df567c11a89
Julio Rivas <rivasjulio@ymail.com>
2023-04-22 18:53:21 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter((driverName)=>
+    driverName.toLowerCase() 
+    === string.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter((driverName) =>
+    driverName.toLowerCase().startsWith(query.toLowerCase())
+    )
+}
+
+function matchName(drivers, name) {
+    return drivers.filter ((drivers) =>
+    drivers.name === name);
+}
